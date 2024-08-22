10770eaaacf190a266ba152f92ff54860fd0bfe0
Tanvee Mehta <48158213+tanveemehta@users.noreply.github.com>
2024-08-23 00:18:25 +0530

    Time: 3 ms (41.07%), Space: 7.4 MB (57.13%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int numBits = (int)(log2(num)+1);
+        
+        for(int i=0;i<numBits;i++){
+            num = num ^ (1<<i);
+        }
+        return num;
+    }
+};
