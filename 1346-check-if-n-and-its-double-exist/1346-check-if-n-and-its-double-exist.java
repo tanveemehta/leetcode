3290a5852698d8bb29a3ea3a9c7d7e9118127396
Tanvee Mehta <48158213+tanveemehta@users.noreply.github.com>
2024-12-02 01:30:21 +0530

    Time: 2 ms (93.43%), Space: 42.7 MB (61.27%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+
+        // Iterate over each pair (i, j)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // If i != j and arr[i] is 2 times arr[j]
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
